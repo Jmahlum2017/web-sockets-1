f56af7ddced6822c03ee191a1edf97b0b2f8e213
J Mahlum <29134513+Jmahlum2017@users.noreply.github.com>
2017-12-01 17:55:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
     
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+    
